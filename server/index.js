217a9c92503a6ab3d1fb86fed90437128132e31d
SANTHOSH R <153372067+Santhosh-636@users.noreply.github.com>
2024-03-31 11:49:03 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin:["https://skolarproject-frontend.vercel.app/register"],
+    origin:["https://skolarproject-frontend.vercel.app/"],
     methods: ["POST" , "GET"],
     credential:true
   }
